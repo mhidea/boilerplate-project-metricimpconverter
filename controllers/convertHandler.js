edd395b2336d15c56a629d859fee8c1b35f86900
MohammadHassan Haghighifard <mhideadesign@gmail.com>
2022-01-16 23:59:16 +0330

    felan

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,60 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.input = null
+  this.getNum = function () {
+    let p = new RegExp('^[^a-z]*(?=[a-z])', 'gi');
+    let result = this.input.match(p);
+    return result[0] ? eval(result[0]) : 1;
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+
+  this.getUnit = function () {
+    let p = new RegExp('[a-z]+\$', 'gi')
+    return this.input.match(p)[0];
   };
-  
-  this.getReturnUnit = function(initUnit) {
+  this.getReturnUnit = function () {
+    return this.units[this.getUnit()].to;
+  };
+
+  this.spellOutUnit = function (unit) {
     let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.convert = function () {
+    return (this.getNum() * this.units[this.getUnit()].convertion).toFixed(5);
   };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+
+  this.getString = function () {
+    return `${this.getNum()} ${this.units[this.getUnit()].name} converts to ${this.convert()} ${this.units[this.getReturnUnit()].name}`;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+
+
+}
+
+ConvertHandler.prototype.units = {
+  gal: { to: "L", name: "gallons", convertion: 3.78541 },
+  L: { to: "gal", name: "liters", convertion: 1 / 3.78541 },
+  mi: { to: "km", name: "miles", convertion: 1.60934 },
+  km: { to: "mi", name: "kilometers", convertion: 1 / 1.60934 },
+  lbs: { to: "kg", name: "pounds", convertion: 0.453592 },
+  kg: { to: "lbs", name: "kilograms", convertion: 1 / 0.45359 }
+}
+ConvertHandler.prototype.work = function (input) {
+  this.input = input;
+  try {
+    return {
+      initNum: this.getNum(),
+      initUnit: this.getUnit(),
+      returnNum: this.convert(),
+      returnUnit: this.getReturnUnit(),
+      string: this.getString()
+    }
+  } catch (error) {
+
+  }
+  return { string: "error" }
 }
 
 module.exports = ConvertHandler;
+
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,42 @@
+const { expect } = require('chai');
 const chai = require('chai');
 let assert = chai.assert;
 const ConvertHandler = require('../controllers/convertHandler.js');
-
+expect(assert, 'k')
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite('Unit Tests', function () {
+    test('convertHandler should correctly read a whole number input.', function () {
+        assert.isNumber(convertHandler.work('25mi').initNum, "Not a whole number input");
+    })
+    test('convertHandler should correctly read a decimal number input.', function () {
+        assert.isNumber(convertHandler.work('25.356mi').initNum, "");
+    })
+    test('convertHandler should correctly read a fractional input.', function () {
+        assert.isNumber(convertHandler.work('25/356mi').initNum, "");
+    })
+    test('convertHandler should correctly read a fractional input with a decimal.', function () {
+        assert.isNumber(convertHandler.work('25.356/53.9mi').initNum, "");
+    })
+    test('convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).', function () {
+        assert.ifError(convertHandler.work('25/5/5mi').initNum, "");
+    })
+    test("convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.", function () {
+        assert.equal(convertHandler.work('mi').initNum, 1, "");
+    })
+    test(
+        "convertHandler should correctly read each valid input unit."
+        , function () {
+            Object.keys(convertHandler.units).forEach(val => {
+                assert.equal(convertHandler.work('25' + val).initUnit, val, "");
+            });
+        })
+    test("convertHandler should correctly return an error for an invalid input unit.", function () {
+        assert.ifError(convertHandler.work('25m'), "");
+    })
+
+
+
+
 
 });
